1686126ce10b1e1902bb73b7c8e3b90a7a5d4d15
sarahT04 <sarahtanujaya@gmail.com>
2024-09-07 22:14:31 +0800

    REFACTOR: added prettier

--- src/calc.js
+++ src/calc.js
@@ -1,18 +1,16 @@
-
-const { Chess } = require('chess.js');
+const { Chess } = require("chess.js");
 
 const extractMoves = (pgn) => {
-    // Remove lines that start with "[" and end with "]"
-    const filtered = pgn.replace(/\[.*?\]\n/g, '').trim();
-    
-    return filtered;
-  };
+  // Remove lines that start with "[" and end with "]"
+  const filtered = pgn.replace(/\[.*?\]\n/g, "").trim();
 
+  return filtered;
+};
 
-const getFen = async (moves)=>{
-    const chess = new Chess()
-    chess.loadPgn(moves)
-    return chess.fen()
-}
+const getFen = async (moves) => {
+  const chess = new Chess();
+  chess.loadPgn(moves);
+  return chess.fen();
+};
 
-module.exports = {getFen, extractMoves}
+module.exports = { getFen, extractMoves };
